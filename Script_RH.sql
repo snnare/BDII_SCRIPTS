ce5930d8d47abb29eb805904a983c17ebe8c10fc
Jose Rios <68526429+snnare@users.noreply.github.com>
2022-05-23 11:49:10 -0500

    Update Script_RH.sql
    RH final

--- Script_RH.sql
+++ Script_RH.sql
@@ -6,7 +6,7 @@ CREATE DATABASE BDII_PROYECTO;
 
 CREATE TABLE direccion(
 	id_direccion			SERIAL,
-	cp						VARCHAR(5) NOT NULL,
+	cp						VARCHAR(5)  NOT NULL,
 	entidad_federativa		VARCHAR(30) NOT NULL, 
 	localidad				VARCHAR(40) NOT NULL,
 	colonia					VARCHAR(50) NOT NULL,
@@ -26,35 +26,30 @@ CREATE TABLE persona (
 	apellido2       VARCHAR(50) NOT NULL,
 	telefono		VARCHAR(10) NOT NULL,
 	email_persona   VARCHAR(45),
-	direccion_id	SERIAL,
+	direccion_id	SERIAL		NOT NULL,
 
 	PRIMARY KEY (rfc),
 	FOREIGN KEY (direccion_id) REFERENCES direccion(id_direccion)
 );
 
 
--- RH
-
--- En esta tabla tenemos una PRIMARY KEY compuesta por (RFC, id_empleado)
--- no obstante si la creamos como tal PRIMARY KEY(rfc, id_empleado) al momento
--- de crear las demas tablas donde id_empleado es una FOREING KEY no nos deja
 CREATE TABLE empleado (
 	id_empleado			INTEGER 	NOT NULL,
-	cuenta_bancaria 	VARCHAR(20) NOT NULL,
+	cuenta_bancaria 	INTEGER 	NOT NULL,
 	nss					VARCHAR(11) NOT NULL,
 	fecha_nacimiento	DATE 		NOT NULL,
 	cv					VARCHAR(40) NOT NULL, -- PATH donde se encuentra el archivo
-	email_trabajo		VARCHAR(45) NOT NULL,
-	licencia 			VARCHAR(15),
+	email_trabajo		VARCHAR(50) NOT NULL,
+	licencia 			INTEGER,
     
-    PRIMARY KEY (id_empleado)
-
+    PRIMARY KEY (rfc, id_empleado)
 )INHERITS (persona);
 
 
 
 CREATE TABLE nomina(
-	id_nomina 		SERIAL,
+	id_nomina 		SERIAL		 NOT NULL,
+	rfc				VARCHAR(13)	 NOT NULL,
 	empleado_id 	INTEGER 	 NOT NULL,
 	fecha_inicio	DATE 		 NOT NULL,
 	fecha_corte		DATE    	 NOT NULL,
@@ -63,19 +58,20 @@ CREATE TABLE nomina(
 	salario_neto	NUMERIC(6,2) NOT NULL,
 
 	PRIMARY KEY (id_nomina),
-	FOREIGN KEY (empleado_id) REFERENCES empleado(id_empleado)
+	FOREIGN KEY (rfc, empleado_id) REFERENCES empleado(rfc, id_empleado)
 );
 
 
 CREATE TABLE dependiente_economico(
-	id_dependiente_economico	SERIAL,
+	id_dependiente_economico	SERIAL 		 NOT NULL,
 	parentesco					VARCHAR(40)  NOT NULL,
 	nombre_completo				VARCHAR(100) NOT NULL,
 	telefono					VARCHAR(10)  NOT NULL,
+	rfc							VARCHAR(13)	 NOT NULL,
 	empleado_id					INTEGER      NOT NULL,
 
 	PRIMARY KEY(id_dependiente_economico),
-	FOREIGN KEY(empleado_id) REFERENCES empleado(id_empleado)
+	FOREIGN KEY(rfc, empleado_id) REFERENCES empleado(rfc, id_empleado)
 );
 
 CREATE TABLE prestacion(
@@ -83,24 +79,23 @@ CREATE TABLE prestacion(
 	nombre 			VARCHAR(40)	 NOT NULL,
 	tipo			VARCHAR(30)  NOT NULL,
 	descripcion		TEXT		 NOT NULL,
-	duracion 		DATE  		 NOT NULL,
+	duracion 		VARCHAR(20)  NOT NULL,
 	monto  			NUMERIC(6,2) NOT NULL,
 
 	PRIMARY KEY(id_prestacion)
 );
 
--- Tabla pivote
 CREATE TABLE prestacion_emp(
+	rfc				VARCHAR(13) NOT NULL,
 	empleado_id		INTEGER 	NOT NULL,
 	prestacion_id	INTEGER 	NOT NULL,
 	fecha_inicio    DATE    	NOT NULL,
 	fecha_corte     DATE,
 
-	PRIMARY KEY (empleado_id, prestacion_id, fecha_inicio),
-	FOREIGN KEY(empleado_id) REFERENCES empleado(id_empleado),
+	PRIMARY KEY (rfc, empleado_id, prestacion_id, fecha_inicio),
+	FOREIGN KEY(rfc, empleado_id) REFERENCES empleado(rfc, id_empleado),
 	FOREIGN KEY(prestacion_id) REFERENCES prestacion(id_prestacion)
 );
-
 
 CREATE TABLE turno(
 	id_turno     	INTEGER NOT NULL,
@@ -112,46 +107,50 @@ CREATE TABLE turno(
 
 
 CREATE TABLE jornada_emp(
-	empleado_id 	INTEGER	NOT NULL,
-	fecha_inicio	DATE    NOT NULL,
+	rfc				VARCHAR(13) 	NOT NULL,
+	empleado_id 	INTEGER			NOT NULL,
+	fecha_inicio	DATE    		NOT NULL,
 	fecha_termino   DATE,
-	turno_id        INTEGER NOT NULL, 
+	turno_id        INTEGER 		NOT NULL, 
 
-	PRIMARY KEY (empleado_id, fecha_inicio),
-	FOREIGN KEY (empleado_id) REFERENCES empleado(id_empleado),
-	FOREIGN KEY (turno_id)	  REFERENCES turno(id_turno)
+	PRIMARY KEY (rfc, empleado_id, fecha_inicio),
+	FOREIGN KEY (rfc, empleado_id) 	REFERENCES empleado(rfc, id_empleado),
+	FOREIGN KEY (turno_id)	  		REFERENCES turno(id_turno)
 );
 
 
 CREATE TABLE curso(
-	id_curso			SERIAL   NOT NULL,
-	descripcion    		TEXT     NOT NULL,
-	duracion_horas		INTEGER  NOT NULL,
+	id_curso			SERIAL   	NOT NULL,
+	descripcion    		TEXT     	NOT NULL,
+	duracion_horas		INTEGER  	NOT NULL,
+	rfc					VARCHAR(13) NOT NULL,
 	empleado_id			INTEGER	 NOT NULL,
 
 	PRIMARY KEY (id_curso),
-	FOREIGN KEY (empleado_id) REFERENCES empleado(id_empleado)
+	FOREIGN KEY (rfc, empleado_id) REFERENCES empleado(rfc, id_empleado)
 );
 
 CREATE TABLE capacitacion(
-	curso_id		SERIAL   NOT NULL,
-	empleado_id		INTEGER	 NOT NULL,
-	fecha           DATE     NOT NULL,
+	curso_id		SERIAL   		NOT NULL,
+	rfc				VARCHAR(13)	 	NOT NULL,
+	empleado_id		INTEGER	 		NOT NULL,
+	fecha           DATE     		NOT NULL,
 
-	PRIMARY KEY(curso_id, empleado_id),
+	PRIMARY KEY(curso_id, rfc, empleado_id),
 	FOREIGN KEY(curso_id) REFERENCES curso (id_curso),
-	FOREIGN KEY(empleado_id) REFERENCES empleado(id_empleado)
+	FOREIGN KEY(rfc, empleado_id) REFERENCES empleado(rfc, id_empleado)
 );
 
 
 CREATE TABLE evaluacion(
+	rfc				 VARCHAR(13)	NOT NULL,
 	empleado_id		 INTEGER 		NOT NULL,	
 	fecha 			 DATE    		NOT NULL,    
 	rating			 VARCHAR(30)	NOT NULL,
-	observaciones    TEXT
+	observaciones    TEXT,
 
-	PRIMARY KEY(empleado_id, fecha),
-	FOREIGN KEY(empleado_id) REFERENCES empleado (id_empleado),
+	PRIMARY KEY(rfc, empleado_id, fecha),
+	FOREIGN KEY(rfc, empleado_id) REFERENCES empleado (rfc, id_empleado)
 );
 
 
@@ -173,21 +172,24 @@ CREATE TABLE cargo(
 );
 
 CREATE TABLE contrato(
+	rfc					VARCHAR(13)	NOT NULL,
 	empleado_id			INTEGER 	NOT NULL,
 	fecha_inicio		DATE 		NOT NULL,
 	fecha_termino		DATE 		NOT NULL,
 	tipo				VARCHAR(20)	NOT NULL,
 	cargo_id		    INTEGER		NOT NULL,
 
-	PRIMARY KEY(empleado_id, fecha_inicio),
-	FOREIGN KEY(empleado_id) REFERENCES empleado (id_empleado),
+	PRIMARY KEY(rfc, empleado_id, fecha_inicio),
+	FOREIGN KEY(rfc, empleado_id) REFERENCES empleado (rfc, id_empleado),
 	FOREIGN KEY(cargo_id) REFERENCES cargo(id_cargo)
 );
 
 CREATE TABLE salario(
-	contrato_id		
-	fecha_inicio
-	salario_base
+	id_salario			SERIAL			NOT NULL, -- Poner restricciones , revisar
+	fecha_inicio		DATE 			NOT NULL,
+	salario_base		NUMERIC(6,2)	NOT NULL,
+
+	PRIMARY KEY(id_salario)
 );
 
 
@@ -204,20 +206,17 @@ CREATE TABLE oficina(
 );
 
 CREATE TABLE acceso(
+	rfc					VARCHAR(13)	NOT NULL,
 	empleado_id			INTEGER 	NOT NULL,
 	fecha_hora_in		TIMESTAMP	NOT NULL,
 	fecha_hora_out		TIMESTAMP   NOT NULL,
 	tipo				VARCHAR(20) NOT NULL,
 	oficina_id          INTEGER		NOT NULL,
 
-	PRIMARY KEY(empleado_id, fecha_hora_in),
-	FOREIGN KEY(empleado_id) REFERENCES empleado(id_empleado),
+	PRIMARY KEY(rfc, empleado_id, fecha_hora_in),
+	FOREIGN KEY(rfc, empleado_id) REFERENCES empleado(rfc, id_empleado),
 	FOREIGN KEY(oficina_id) REFERENCES oficina (id_oficina)
 );
-
-
-
-
 
 CREATE TABLE candidato(
 	id_candidato 		SERIAL NOT NULL,
@@ -231,9 +230,26 @@ CREATE TABLE candidato(
 
 ) INHERITS (persona);
 
+CREATE TABLE vacante (
+	id_vacante				SERIAL 			NOT NULL,
+	cargo_id				INTEGER			NOT NULL,
+	fecha_apertura			DATE 			NOT NULL,
+	estudios_requeridos		VARCHAR(30)		NOT NULL,
+	experiencia_requerida   INTEGER 		NOT NULL,
+	status 					VARCHAR(20)		NOT NULL,
+
+	PRIMARY KEY(id_vacante),
+	FOREIGN KEY(cargo_id) REFERENCES cargo(id_cargo)
+);
+
 CREATE TABLE postulacion(
-	candidato_id	  	SERIAL NOT NULL,
-	vacante_id			
-	rating
-	status			
+	rfc					VARCHAR(13)		NOT NULL,
+	candidato_id	  	SERIAL 			NOT NULL,
+	vacante_id			SERIAL 			NOT NULL,
+	rating				INTEGER			NOT NULL,		
+	status				VARCHAR(30)		NOT NULL,
+
+	PRIMARY KEY (rfc, candidato_id, vacante_id),
+	FOREIGN KEY (rfc, candidato_id) REFERENCES candidato(rfc, id_candidato),
+	FOREIGN KEY (vacante_id) REFERENCES vacante(id_vacante)
 );
