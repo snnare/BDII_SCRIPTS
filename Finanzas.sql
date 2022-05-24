2151612d63800f654db38b176a0587d4cc700027
Brian Raamiireezz <Brian_Raamiireezz@hotmail.com>
2022-05-23 20:36:07 -0500

    Act FK ventas

--- Finanzas.sql
+++ Finanzas.sql
@@ -3,6 +3,6 @@ CREATE TABLE FACTURA (
 	folio_factura SERIAL NOT NULL PRIMARY KEY,
 	fecha_emision DATE NOT NULL,
 	lugar_emision VARCHAR(50) NOT NULL,
-	venta_id INTEGER REFERENCES VENTA(manufactura_id)
+	venta_id INTEGER NOT NULL REFERENCES VENTA(manufactura_id)
 
 );
