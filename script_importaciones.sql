cff887e2971ac3632c3e228c0ba923ed96cc6eb4
Jose Rios <roraj77@hotmail.com>
2022-05-23 23:20:20 -0500

    corrigiendo importaciones

--- script_importaciones.sql
+++ script_importaciones.sql
@@ -1,8 +1,8 @@
 --IMPORTACIONES
 
 CREATE TABLE proveedor(
-	id_proveedor		SERIAL,
-	nombre			VARCHAR(50) NOT NULL,
+	id_proveedor		SERIAL 		NOT NULL,
+	nombre				VARCHAR(50) NOT NULL,
 	nacionalidad		VARCHAR(30) NOT NULL,
 	info_contacto		TEXT NOT NULL,
 
@@ -11,47 +11,48 @@ CREATE TABLE proveedor(
 
 
 CREATE TABLE importacion (
-	id_importacion		SERIAL,
+	id_importacion		SERIAL		 NOT NULL,
 	cuota_arancelaria	NUMERIC(6,2) NOT NULL,
-	fecha_recepcion		DATE NOT NULL,
-	total      		NUMERIC(6,2) NOT NULL,
+	fecha_recepcion		DATE 		 NOT NULL,
+	total      			NUMERIC(6,2) NOT NULL,
 
 	PRIMARY KEY (id_importacion)
 );
 
 
 CREATE TABLE pedido (
-	id_pedido		SERIAL,
-	proveedor_id 		SERIAL	NOT NULL,
-	importacion_id   	SERIAL	NOT NULL,
-	empleado_id 		INTEGER	NOT NULL,
-	fecha_pedido		DATE	NOT NULL,
-	fecha_recepcion		DATE	NOT NULL,
-	total 			NUMERIC(6,2) NOT NULL,
+	id_pedido			SERIAL	 	NOT NULL,
+	proveedor_id 		SERIAL	 	NOT NULL,
+	importacion_id   	SERIAL	 	NOT NULL,
+	rfc					VARCHAR(13) NOT NULL,
+	empleado_id 		INTEGER	 	NOT NULL,
+	fecha_pedido		DATE	 	NOT NULL,
+	fecha_recepcion		DATE	 	NOT NULL,
+	total 				NUMERIC(6,2)NOT NULL,
 
     PRIMARY KEY (id_pedido),
-    FOREIGN KEY (PROVEEDOR_proveedor_id) 	REFERENCES proveedor(id_proveedor),
-    FOREIGN KEY (IMPORTACION_importacion_id) 	REFERENCES importacion(id_importacion),
-    FOREIGN KEY (EMPLEADO_empleado_id) 		REFERENCES empleado(id_empleado)
+    FOREIGN KEY (proveedor_id) 		REFERENCES proveedor(id_proveedor),
+    FOREIGN KEY (importacion_id) 	REFERENCES importacion(id_importacion),
+    FOREIGN KEY (rfc, empleado_id) 	REFERENCES empleado(rfc, id_empleado)
 
 );
 
 
 
 CREATE TABLE producto_pedido(
-	id_producto 	SERIAL,
-	producto_id 	SERIAL NOT NULL,
-	pedido_id	SERIAL NOT NULL,
-	costo   	NUMERIC(6,2) NOT NULL,
+	id_producto_pedido 		SERIAL 			NOT NULL,
+	producto_id 			SERIAL 			NOT NULL,
+	pedido_id				SERIAL 			NOT NULL,
+	costo   				NUMERIC(6,2) 	NOT NULL,
 
-	PRIMARY KEY (id_producto),
-	FOREIGN KEY (PRODUCTO_producto_id) 	REFERENCES producto(id_producto),
-      	FOREIGN KEY (PEDIDO_pedidio_id) 	REFERENCES pedido(id_pedido)
+	PRIMARY KEY (id_producto_pedido),
+	FOREIGN KEY (producto_id) 	REFERENCES producto(id_producto),
+    FOREIGN KEY (pedido_id) 	REFERENCES pedido(id_pedido)
 );
 
 
 CREATE TABLE producto(
-	id_producto 	SERIAL,
+	id_producto 		SERIAL		NOT NULL,
 	nombre 		VARCHAR(50) NOT NULL,
 	descripcion 	VARCHAR(80) NOT NULL,
 	unidad 		INTEGER NOT NULL,
