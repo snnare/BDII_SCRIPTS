00573142d3a86e8b0831dcaf6da6c897534cc7fd
Brian Raamiireezz <Brian_Raamiireezz@hotmail.com>
2022-05-23 20:19:17 -0500

    Pre-Finanzas

---
+++ Finanzas.sql
@@ -0,0 +1,8 @@
+CREATE TABLE FACTURA (
+
+	folio_factura SERIAL NOT NULL PRIMARY KEY,
+	fecha_emision DATE NOT NULL,
+	lugar_emision VARCHAR(50) NOT NULL,
+	venta_id VARCHAR() REFERENCES VENTA(manufactura_id)
+
+);
