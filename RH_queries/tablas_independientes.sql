351bea9ee59e670cf8083c793d53f24357dca5cb
Jose Rios <roraj77@hotmail.com>
2022-05-27 15:11:34 -0500

    agregando prestaciones

--- RH_queries/tablas_independientes.sql
+++ RH_queries/tablas_independientes.sql
@@ -49,9 +49,32 @@ INSERT INTO cargo VALUES (n, 'Analista','Encargado de analizar informacion del m
 ----------- PRESTACIONES
 --- LEY
 --- Checar si cambiar "duracion" de DATE a VARCHAR
-INSERT INTO prestacion VALUES (1, 'vacaciones','ley','disfrutar en forma continua 6 días de vacaciones, por lo menos.', '2021/03/18', 650.30 )
-INSERT INTO prestacion VALUES (2, 'aguinaldo', 'ley', 'un aguinaldo anual que deberá pagarse antes del día 20 de diciembre, equivalente a quince días de salario, por lo menos.','2021/03/18', )
-INSERT INTO prestacion VALUES (id_prestacion, nombre, tipo , descripcion, duracion, monto)
+INSERT INTO prestacion VALUES (1, 'Vacaciones','ley','Disfrutar en forma continua 6 días de vacaciones, por lo menos.' );
+INSERT INTO prestacion VALUES (2, 'Aguinaldo', 'ley', 'Un aguinaldo anual que deberá pagarse antes del día 20 de diciembre, equivalente a quince días de salario, por lo menos.','2021/03/18', );
+INSERT INTO prestacion VALUES (3, 'Prima vacacional', 'ley', 'Se entrega la prima vacacional después de que cumples el año de servicio, y debe ser de acuerdo a los días de descanso que te correspondan.' );
+INSERT INTO prestacion VALUES (4, 'Seguridad social', 'ley', 'Atención médica, hospitalaria, farmacéutica y rehabilitación para el trabajador y su familia, ahorro para el retiro.');
+INSERT INTO prestacion VALUES (5, 'Utilidades', 'ley', 'Es aplicable a los trabajadores que hayan laborado por lo menos 60 días en empresas cuyas utilidades netas fueron de $300 mil pesos o más' );
+INSERT INTO prestacion VALUES (6, 'Prima dominical', 'ley', 'Monto adicional que se paga al trabajador por laborar el día domingo, cuando tenga como descanso cualquier otro día de la semana');
+INSERT INTO prestacion VALUES (7, 'Licencia de maternidad', 'ley', 'Abarca unas semanas previas al nacimiento y algunas posteriores, en el que la mujer puede ausentarse de su trabajo para cuidar y atender a su hijo que está por nacer o que recién ha nacido, sin perder el pago salarial.' );
+INSERT INTO prestacion VALUES (8, 'Licencia de adopción', 'ley', 'Las madres trabajadoras disfrutarán de un descanso de 6 semanas con goce de sueldo, posteriores al día en que lo reciban.' );
+INSERT INTO prestacion VALUES (9, 'Prima de antigüedad', 'ley', 'Importe de 12 días de salario, por cada año de servicios; en caso de que no se labore el año completo, se pagará la parte proporcional, de acuerdo al tiempo trabajado durante el año.' );
+INSERT INTO prestacion VALUES (10, 'Licencia de paternidad', 'ley', 'Se otorga un permiso de paternidad de cinco días laborables con goce de sueldo, a los trabajadores, por el nacimiento de sus hijos' );
+INSERT INTO prestacion VALUES (11, 'Renuncia', 'ley', 'Acto libre y unilateral de la voluntad del trabajador, por el que decide dar por terminada la relación laboral.' );
+INSERT INTO prestacion VALUES (12, 'Despido', 'ley', 'El patrón rescindirá en cualquier tiempo la relación de trabajo, por causa justificada, sin incurrir en responsabilidad.' );
+INSERT INTO prestacion VALUES (13, 'Seguro de vida', 'ley superior', 'Después del fallecimiento de un trabajador se protegerá financieramente a su familia y a otras personas que dependen de sus ingresos.' );
+INSERT INTO prestacion VALUES (14, 'Vales de despensa', 'ley superior', 'Apoyo para la adquisición de diferentes artículos de consumo; como despensa, artículos de limpieza, útiles escolares, electrodomésticos, electrónicos, ropa y calzado.');
+INSERT INTO prestacion VALUES (15, 'Gastos médicos', 'ley superior', 'Cubre los gastos de hospitalización con atención médica, intervenciones quirúrgicas, medicamentos y análisis clínicos, entre otros servicios profesionales.' );
+INSERT INTO prestacion VALUES (16, 'Caja de ahorro', 'Se fomenta el ahorro entre los trabajadores para promover el bienestar económico.' );
+INSERT INTO prestacion VALUES (17, 'Fondo de ahorro', 'ley superior', 'Descuento del salario del trabajador mensualmente una parte y la otra es otorgada el patrón para ahorrar una cantidad inferior al 13% de forma anual.' );
+INSERT INTO prestacion VALUES (18, 'Bonos de productividad', 'bono', 'Remuneración adicional recibida por el trabajador cuando alcanza o supera las metas de desempeño establecidas por la organización, como parte de su plan de crecimiento.' );
+INSERT INTO prestacion VALUES (19, 'Bonos de puntualidad', 'bono', 'Se otorgarán para incentivar a los trabajadores a ser más puntuales. ' );
+
+
+
+
+
+
+
 
 
 ----------- TURNO
